b76cce115ce9a0e6822faac80435d39500a4089f
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2023-06-21 20:26:59 -0300

    Adicionando base teste Alurapic

---
+++ Primeiro_Projeto/cypress/e2e/Alurapic/Alurapic.cy.js
@@ -0,0 +1,9 @@
+describe('Login e registro Alurapic', () => {
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com')
+    })
+
+    it('Verifica mensagens validacao', () => {
+        
+    })
+})
