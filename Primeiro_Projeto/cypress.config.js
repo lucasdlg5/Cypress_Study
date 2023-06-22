8189fd0d0f8af51bc36734ee311d562f5bf1f057
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2023-06-22 10:26:33 -0300

    Removendo configuração extra mochawesome

--- Primeiro_Projeto/cypress.config.js
+++ Primeiro_Projeto/cypress.config.js
@@ -7,10 +7,7 @@ module.exports = defineConfig({
     "overwrite": true,
     "html": true,
     "json": false,
-    "timestamp": "mmddyyyy_HHMMss",
-    "charts": true,
-    "reportTitle": "Alura: Automatização E2E com Cypress",
-    "code": true
+    "timestamp": "mmddyyyy_HHMMss"
     },
     projectId: "jvk4n6",
 
