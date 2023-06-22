f6c5d3d18079a3f3966af9d88af5c8f347051751
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2023-06-22 00:20:55 -0300

    Adicionando Mochawesome + Cypress Cloud

--- Primeiro_Projeto/cypress.config.js
+++ Primeiro_Projeto/cypress.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  "reporterOptions": {
+    "reportDir": "cypress/report/mochawesome-report",
+    "overwrite": true,
+    "html": true,
+    "json": false,
+    "timestamp": "mmddyyyy_HHMMss"
+    },
+    projectId: "jvk4n6",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
