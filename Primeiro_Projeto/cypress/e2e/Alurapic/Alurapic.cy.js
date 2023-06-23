9cce9a6fc1968bf438e75d854fe7b334e1b75a97
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2023-06-22 22:00:38 -0300

    Testes com massa de dados

--- Primeiro_Projeto/cypress/e2e/Alurapic/Alurapic.cy.js
+++ Primeiro_Projeto/cypress/e2e/Alurapic/Alurapic.cy.js
@@ -18,7 +18,6 @@ describe('Login e registro Alurapic', () => {
         cy.contains('button', 'Register').click();
         cy.get('input[formcontrolname="email"]').type('Lusga'); //Type é para escrita, não tipo!!!
         cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
-        
     })
 
     it('Verifica mensagem de Nome de usuario invalido', () => {
@@ -26,7 +25,6 @@ describe('Login e registro Alurapic', () => {
         cy.get('input[formcontrolname="userName"]').type('a'); //Type é para escrita, não tipo!!!
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
-        
     })
 
     it('Verifica mensagem de Nome de usuario invalido - Extrapolado', () => {
@@ -34,7 +32,6 @@ describe('Login e registro Alurapic', () => {
         cy.get('input[formcontrolname="userName"]').type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'); //Type é para escrita, não tipo!!!
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Maximun length is 30').should('be.visible');
-        
     })
 
     it('Verifica mensagem de Nome de usuario invalido - Usuario ja utilizado', () => {
@@ -42,7 +39,6 @@ describe('Login e registro Alurapic', () => {
         cy.get('input[formcontrolname="userName"]').type('flavio'); //Type é para escrita, não tipo!!!
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Username already taken').should('be.visible');
-        
     })
 
     it('Verifica mensagem de Senha invalido', () => {
@@ -50,7 +46,6 @@ describe('Login e registro Alurapic', () => {
         cy.get('input[formcontrolname="password"]').type('a'); //Type é para escrita, não tipo!!!
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
-        
     })
 
     it('Verifica mensagem de Senha invalido - Extrapolado', () => {
@@ -58,19 +53,31 @@ describe('Login e registro Alurapic', () => {
         cy.get('input[formcontrolname="password"]').type('aaaaaaaaaaaaaaaaaaa'); //Type é para escrita, não tipo!!!
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Maximun length is 18').should('be.visible');
-        
     })
 
-
-        it('Fazer login de usuário VÁLIDO', () => {
+    it('Fazer login de usuário VÁLIDO', () => {
         cy.login('flavio','123') // Biblioteca criada em Support/gui_commands.js
         cy.contains('a','(Logout)').should('be.visible');
-        })
+    })
 
-        it('Fazer login de usuário INVÁLIDO', () => {
-            cy.login('Lusga','1234') // Biblioteca criada em Support/gui_commands.js
-            cy.on('window.alert', (str) => {
+    it('Fazer login de usuário INVÁLIDO', () => {
+        cy.login('Lusga','1234') // Biblioteca criada em Support/gui_commands.js
+        cy.on('window.alert', (str) => {
                 expect(str).to.equal('Invalid user name or password')
             })
         })
+const usuarios = require ('../../fixtures/usuarios.json');
+usuarios.forEach(usuario => {
+    it.only(`Registra Novo Usuario - ${usuario.userName}`, () => {
+        cy.contains('a','Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"').type(usuario.email);
+        cy.get('input[formcontrolname="fullName"').type(usuario.fullName);
+        cy.get('input[formcontrolname="userName"').type(usuario.userName);
+        cy.get('input[formcontrolname="password"').type(usuario.password);
+        cy.contains('button', 'Register').click();
+    })
+})
+    
+    
     }) 
